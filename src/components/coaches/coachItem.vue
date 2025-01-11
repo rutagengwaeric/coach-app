b3340c355e4c10a92e60d62caebf318824d172f5
Rutagengwa Eric <rutagebanke@gmail.com>
2025-01-11 17:49:38 +0200

    add CoachItem

---
+++ src/components/coaches/coachItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <li>
+    <h3> {{ coach.firstName }} </h3>
+    <h4> {{ coach.hourlyRate }}/hour</h4>
+    <div>
+      <span v-for="area in coach.areas" :key="area"> {{ area }}, </span>
+    </div>
+    <div class="actions">
+      <router-link to="/coaches/c1/contact">Contact</router-link>
+      <router-link to="/coaches/c1">View <Details></Details></router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default{
+
+props: ['coach'],
+
+}
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/pages/coach/CoachList.vue
+++ src/pages/coach/CoachList.vue
@@ -1,29 +1,51 @@
 <template>
-        <section>
-            FILTER
-        </section>
+  <section>
+    <div class="controls">
+      FILTER
+    </div>
+  </section>
 
-        <section>
-            LIST OF COACHES 
-            <ul v-if="hasCoaches">
-                <li v-for="coaches in filteredCoaches" :key="coaches.id">
-                    <h2> {{ coaches.firstName }} </h2>
-                </li>
-            </ul>
-            <p v-else>No coaches found</p>
-        </section>
+  <section>
+    LIST OF COACHES
+    <ul v-if="hasCoaches">
+      <coach-item v-for="coach in filteredCoaches" :key="coach.id" :coach="coach">
+
+      </coach-item>
+    </ul>
+    <p v-else>No coaches found</p>
+  </section>
 </template>
 
 <script>
-export default{
-    computed : {
-        filteredCoaches(){
-            return this.$store.getters['coaches/coaches'];
-        },
-        hasCoaches(){
-            return this.$store.getters['coaches/hasCoaches'];
-        }
+
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
+export default {
+  components: {
+    CoachItem
+  }
+,
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches'];
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches'];
     }
+  }
 }
 
 </script>
+
+<style>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
